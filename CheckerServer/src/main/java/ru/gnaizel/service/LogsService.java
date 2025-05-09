6d74a2286ae6bbaa3f99ae920bc78ad2209b809e
Gnaiz <Gnaizel@mail.ru>
2025-05-09 23:06:50 +0300

    Revert "Add upload file function"
    This reverts commit bb75f25990c1c6bdbf5bdc5f7dd28a2b93543dbc.

--- CheckerServer/src/main/java/ru/gnaizel/service/LogsService.java
+++
@@ -1,9 +0,0 @@
-package ru.gnaizel.service;
-
-import org.springframework.web.multipart.MultipartFile;
-import ru.gnaizel.dto.log.LogDto;
-
-public interface LogsService {
-
-    LogDto uploadLog(MultipartFile file, long telegramId);
-}
