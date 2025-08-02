2644555aea405ba64443858f62fc4105c1126bc3
Gnaizel <Gnaizel@mail.ru>
2025-08-02 17:46:51 +0300

    Add UploadService

--- AssetMenagger/ScripteTG/src/resources/data/config.py
+++ AssetMenagger/ScripteTG/src/resources/data/config.py
@@ -1,5 +1,5 @@
 # токен бота
-BOT_TOKEN = "FFFFF"
+BOT_TOKEN = "7251406246:AAEEX6gizlk5j2-l_TcmDm7xeYZ6d_-qg-4"
 # префикс
 COMMAND_PREFIX = "."
 # самоуничтожающиеся медиа
--- AssetMenagger/UploadService/src/main/java/ru/gnaizel/service/UploadServiceImpl.java
+++ AssetMenagger/UploadService/src/main/java/ru/gnaizel/service/UploadServiceImpl.java
@@ -1,18 +1,49 @@
 package ru.gnaizel.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.gnaizel.client.Client;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UploadServiceImpl implements UploadService {
     private final Client client;
 
-    @Scheduled(fixedDelay = 10800000)
+    @Value("${upload-value.file-path}")
+    private String filePath;
+
+    @Scheduled(fixedDelay = 60) //10800000
     @Override
     public void uploadFiles() {
+        Path path = Paths.get(filePath);
+        List<Path> files;
+        try  {
+            files = Files.list(path).toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
 
+        if (files.isEmpty()) {
+            throw new RuntimeException(filePath + " is empty");
+        }
+
+        for (Path pathFile : files) {
+            File file = pathFile.toFile();
+
+            if (client.uploadLog(file).equals(HttpStatusCode.valueOf(201))) {
+                file.delete();
+            }
+        }
     }
 }
